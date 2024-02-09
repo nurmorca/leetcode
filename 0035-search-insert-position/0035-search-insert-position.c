96fad4c59cfdd4861fa07cf5e355a2d23e5928b5
Hira Nur Morca <105250031+nurmorca@users.noreply.github.com>
2024-02-09 16:09:51 +0300

    Time: 2 ms (83.25%), Space: 6 MB (76.74%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.c
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -1,8 +1,6 @@
 int searchInsert(int* nums, int numsSize, int target) {
     int i = 0, notfound = 1;
     
-    int output = 0;
-    
     while(i < numsSize && notfound){
         if(nums[i] < target)
             i++;
